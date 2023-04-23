5c6161fea238d6db3eabbb8ff2c0fe064c735d3e
orelzam <orelzx13@gmail.com>
2023-04-23 18:59:04 +0300

    initial thread pool

--- try.c
+++ try.c
@@ -4,9 +4,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-
-
-#define MAX_THREADS 8
+#define MAX_THREADS 32
 #define BLOCK_SIZE 1024
 
 typedef struct {
@@ -71,48 +69,7 @@ void* worker(void *arg) {
     return NULL;
 }
 
-
-int main(int argc, char *argv[])
-{
-	// if (argc != 2)
-	// {
-	//     printf("usage: key < file \n");
-	//     printf("!! data more than 1024 char will be ignored !!\n");
-	//     return 0;
-	// }
-
-	// int key = atoi(argv[1]);
-	// printf("key is %i \n",key);
-
-	// char c;
-	// int counter = 0;
-	// int dest_size = 1024;
-	// char data[dest_size]; 
-	
-
-	// while ((c = getchar()) != EOF)
-	// {
-	//   data[counter] = c;
-	//   counter++;
-
-	//   if (counter == 1024){
-	// 	encrypt(data,key);
-	// 	printf("encripted data: %s\n",data);
-	// 	counter = 0;
-	//   }
-	// }
-	
-	// if (counter > 0)
-	// {
-	// 	char lastData[counter];
-	// 	lastData[0] = '\0';
-	// 	strncat(lastData, data, counter);
-	// 	encrypt(lastData,key);
-	// 	printf("encripted data:\n %s\n",lastData);
-	// }
-
-	// return 0;
-
+int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("usage: key < file \n");
         printf("!! data more than 1024 char will be ignored !!\n");
@@ -159,6 +116,4 @@ int main(int argc, char *argv[])
     destroy_thread_pool(&pool);
 
     return 0;
-
-
 }
