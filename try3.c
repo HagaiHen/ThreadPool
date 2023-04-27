6c35171fe40104d36cdd03ca70914c16ca7d29e2
orelzam <orelzx13@gmail.com>
2023-04-27 21:39:05 +0300

    v4

--- try3.c
+++ try3.c
@@ -30,6 +30,22 @@ int wake_free_thread(ThreadPool* pool){
     }
 }
 
+void wake_all_thread(ThreadPool* pool){
+    while (pool->queue->size > 0)
+    {
+        for (int i = 0; i < pool->pool_size; i++)
+        {
+            pthread_mutex_lock(&(pool->threads[i].bussy_lock));
+            if (pool->threads[i].is_bussy == 0)
+            {
+                pool->threads[i].is_bussy = 1;
+                signal_thread(pool, i);
+            }
+            pthread_mutex_unlock(&(pool->threads[i].bussy_lock));
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("usage: key < file \n");
@@ -54,7 +70,7 @@ int main(int argc, char *argv[]) {
         block_data[counter++] = c;
         if (counter == BLOCK_SIZE) {
             enqueue(pool->queue, block_data, key, ENCRYPT);  // need to know if its encrypt or decrypt <<<<<<<<--------------------------------------
-            wake_free_thread(pool);
+            // wake_free_thread(pool);
             counter = 0;
             block_data[0] = '\0';
             printf("\ngot a new job!\n");
@@ -66,8 +82,9 @@ int main(int argc, char *argv[]) {
     // Check if there is any data left to encrypt
     if (counter > 0) {
         enqueue(pool->queue, block_data, key, ENCRYPT);  // need to know if its encrypt or decrypt <<<<<<<<--------------------------------------
-        pthread_cond_signal(&pool->cond);
+        // pthread_cond_signal(&pool->cond);
     }
+    wake_all_thread(pool);
     printf("\nfinished22222222222222222222!!!!!!!\n");
     fflush(stdout);
     // printf("\nout1\n");
