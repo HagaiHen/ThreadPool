62278a1a3f84c596771b729fb94d9bb1606a5c2c
orelzam <orelzx13@gmail.com>
2023-04-27 21:16:34 +0300

    v3

--- thread_pool.h
+++ thread_pool.h
@@ -2,13 +2,29 @@
 #include "queue.h"
 #include <unistd.h>
 
+
+typedef struct my_thread {
+    int index;
+    pthread_t thread;
+    pthread_cond_t cond; // array of condition variables, one for each thread
+    pthread_mutex_t bussy_lock;
+    int is_bussy; // free = 0, bussy = 1
+}my_thread;
+
 typedef struct ThreadPool{
     Queue *queue;
-    pthread_t* threads;
+    my_thread* threads;
     int pool_size;
     pthread_mutex_t lock;
     pthread_cond_t cond;
 } ThreadPool;
+
+
+typedef struct ThreadHandlerArg {
+    int index;
+    ThreadPool* pool;
+}ThreadHandlerArg;
+
 
 void print_task(Task* my_task){
 
@@ -44,17 +60,20 @@ void handle_task(ThreadPool* pool){
 
 void* thread_handler(void* arg) {
     
-    ThreadPool* pool = (ThreadPool*) arg;
+    ThreadHandlerArg* argPool = (ThreadHandlerArg*) arg;
+    ThreadPool* pool = argPool->pool;
+    int index = argPool->index;
+    // printf("\n\npid = %ld\n\nthread = %ld\n\n",pthread_self(),pool->threads[index].thread);
     
     while (1)
     {
-        // pthread_mutex_lock(&pool->queue->lock);
-        while (pool->queue->size == 0)
+        pthread_cond_wait(&pool->threads[index].cond, &pool->lock);
+        if (pool->queue->size > 0)
         {
-            pthread_cond_wait(&pool->cond, &pool->lock);
+            handle_task(pool);
+            printf("Thread %ld finished.\n",(long)pthread_self());
+            pool->threads[index].is_bussy = 0;
         }
-        handle_task(pool);
-        printf("Thread %ld finished.\n",(long)pthread_self());
         // pthread_mutex_unlock(&pool->queue->lock);
     }
 }
@@ -62,13 +81,22 @@ void* thread_handler(void* arg) {
 void init_thread_pool(ThreadPool *pool) {
     pool->queue = (Queue*)malloc(sizeof(Queue));
     init_queue(pool->queue);
-    pool->pool_size = (int)sysconf(_SC_NPROCESSORS_CONF); // number of cores
+    pool->pool_size = ((int)sysconf(_SC_NPROCESSORS_ONLN) * 2)  - 1; // number of cores
     printf("I have %d cores\n", pool->pool_size);
-    pool->threads = (pthread_t*)malloc(pool->pool_size * (sizeof(pthread_t)));
+    pool->threads = (my_thread*)malloc(pool->pool_size * (sizeof(my_thread)));
     for (int i = 0; i < pool->pool_size; i++)
     {
-        pthread_create(&pool->threads[i], NULL, thread_handler, pool);
-        pthread_detach(pool->threads[i]);
+        pool->threads[i].is_bussy = 0;
+        pthread_cond_init(&(pool->threads[i].cond), NULL);
+        pthread_mutex_init(&pool->threads[i].bussy_lock, NULL);
+        pool->threads[i].index = i;
+        ThreadHandlerArg* arg = (ThreadHandlerArg*)malloc(sizeof(ThreadHandlerArg));
+        arg->index = i;
+        arg->pool = pool;
+        pthread_create(&pool->threads[i].thread, NULL, thread_handler, arg);
+
+
+        // pthread_detach(pool->threads[i]);
     }
     
     pthread_mutex_init(&pool->lock, NULL);
--- try3.c
+++ try3.c
@@ -4,6 +4,32 @@
 #define BLOCK_SIZE 1024
 
 
+void signal_thread(ThreadPool* pool, int thread_index) {
+    pthread_mutex_lock(&(pool->lock));
+    pthread_cond_signal(&(pool->threads[thread_index].cond));
+    pthread_mutex_unlock(&(pool->lock));
+}
+
+int wake_free_thread(ThreadPool* pool){
+    int assigned = 0;
+    while (assigned == 0)
+    {
+        for (int i = 0; i < pool->pool_size; i++)
+        {
+            pthread_mutex_lock(&(pool->threads[i].bussy_lock));
+            if (pool->threads[i].is_bussy == 0)
+            {
+                pool->threads[i].is_bussy = 1;
+                assigned = 1;
+                signal_thread(pool, i);
+                pthread_mutex_unlock(&(pool->threads[i].bussy_lock));
+                return i;
+            }
+            pthread_mutex_unlock(&(pool->threads[i].bussy_lock));
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("usage: key < file \n");
@@ -28,26 +54,29 @@ int main(int argc, char *argv[]) {
         block_data[counter++] = c;
         if (counter == BLOCK_SIZE) {
             enqueue(pool->queue, block_data, key, ENCRYPT);  // need to know if its encrypt or decrypt <<<<<<<<--------------------------------------
-            pthread_cond_signal(&pool->cond);
+            wake_free_thread(pool);
             counter = 0;
             block_data[0] = '\0';
             printf("\ngot a new job!\n");
             fflush(stdout);
         }
     }
-    // printf("\nout\n");
+    printf("\nfinished1111111111111111111!!!!!!!\n");
+    fflush(stdout);
     // Check if there is any data left to encrypt
     if (counter > 0) {
         enqueue(pool->queue, block_data, key, ENCRYPT);  // need to know if its encrypt or decrypt <<<<<<<<--------------------------------------
         pthread_cond_signal(&pool->cond);
     }
+    printf("\nfinished22222222222222222222!!!!!!!\n");
+    fflush(stdout);
     // printf("\nout1\n");
     // Wait for the threads to finish processing all the data
     for (int i = 0; i < pool->pool_size; i++) {
-        pthread_join(pool->threads[i], NULL);
+        pthread_join(pool->threads[i].thread, NULL);
     }
-    // printf("\nout2\n");
-
+    printf("\nfinished\n");
+    fflush(stdout);
 
 //  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< need to wait for everyone(threads) to finish
 
